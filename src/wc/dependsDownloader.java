c7ff1d876c91991a10e46abf836cb5d7032638ae
Mahiro Takeshita <103738544+Mahiro-xi@users.noreply.github.com>
2024-07-29 19:43:40 +0900

    URLからURIに変更: 非推奨のjava.net.URLをjava.net.URIに置き換え

--- src/wc/dependsDownloader.java
+++ src/wc/dependsDownloader.java
@@ -3,6 +3,7 @@ package wc;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.util.Map;
 
@@ -32,29 +33,29 @@ public class dependsDownloader {
 	        this.channels = channels;
 	    }
 	
-	public static StringBuffer getjson() throws Exception{
-		URL url = new URL("https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.connect();
-		
-		int responseCode = conn.getResponseCode();
-		if(responseCode != 200) {
-			throw new RuntimeException("HttpResponseCode: " + responseCode);
-		} else {
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String inputLine;
-			StringBuffer content = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				content.append(inputLine);
-			}
-			
-			in.close();
-			conn.disconnect();
-			return content;
-		}
-		
-		}
+	    public static StringBuffer getjson() throws Exception {
+	        URI uri = new URI("https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json");
+	        URL url = uri.toURL();
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.connect();
+	        
+	        int responseCode = conn.getResponseCode();
+	        if (responseCode != 200) {
+	            throw new RuntimeException("HttpResponseCode: " + responseCode);
+	        } else {
+	            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	            String inputLine;
+	            StringBuffer content = new StringBuffer();
+	            while ((inputLine = in.readLine()) != null) {
+	                content.append(inputLine);
+	            }
+	            
+	            in.close();
+	            conn.disconnect();
+	            return content;
+	        }
+	    }
 	
 	public static void someget() throws Exception {
 			try {
